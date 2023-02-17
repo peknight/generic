3d4cabd10635384844a1b9341a0387e8ca666e9b
peknight <JKpeknight@gmail.com>
2023-02-17 09:16:27 +0800

    optimize test case

--- generic-core/shared/src/test/scala/com/peknight/generic/deriving/CsvEncoder.scala
+++ generic-core/shared/src/test/scala/com/peknight/generic/deriving/CsvEncoder.scala
@@ -11,15 +11,21 @@ object CsvEncoder:
   def apply[A](using enc: CsvEncoder[A]): CsvEncoder[A] = enc
   def instance[A](func: A => List[String]): CsvEncoder[A] = func(_)
 
-  given productInstance[A](using generic: => Generic.Product[CsvEncoder, A]): CsvEncoder[A] with
-    def encode(value: A): List[String] =
-      generic.map[[_] =>> List[String]](value)([T] => (ft: CsvEncoder[T], t: T) => ft.encode(t))
-        .foldRight(List.empty[String])([T] => (t: T, b: List[String]) => t.asInstanceOf[List[String]] ::: b)
-  end productInstance
+  // given productInstance[A](using generic: => Generic.Product[CsvEncoder, A]): CsvEncoder[A] with
+  //   def encode(value: A): List[String] =
+  //     generic.map[[_] =>> List[String]](value)([T] => (ft: CsvEncoder[T], t: T) => ft.encode(t))
+  //       .foldRight(List.empty[String])([T] => (t: T, b: List[String]) => t.asInstanceOf[List[String]] ::: b)
+  // end productInstance
 
-  given sumInstance[A](using generic: => Generic.Sum[CsvEncoder, A]): CsvEncoder[A] with
-    def encode(value: A): List[String] = generic.instance(value).encode(value)
-  end sumInstance
+  // given sumInstance[A](using generic: => Generic.Sum[CsvEncoder, A]): CsvEncoder[A] with
+  //   def encode(value: A): List[String] = generic.instance(value).encode(value)
+  // end sumInstance
+
+  inline given derived[A](using generic: => Generic[CsvEncoder, A]): CsvEncoder[A] = generic.derive(
+    gen ?=> instance(value => gen.map[[_] =>> List[String]](value)([T] => (ft: CsvEncoder[T], t: T) => ft.encode(t))
+      .foldRight(List.empty[String])([T] => (t: T, b: List[String]) => t.asInstanceOf[List[String]] ::: b)),
+    gen ?=> instance(value => gen.instance(value).encode(value))
+  )
 
   given CsvEncoder[String] with
     def encode(value: String): List[String] = List(value)
--- generic-scalacheck/shared/src/main/scala/com/peknight/generic/scalacheck/instances/ArbitraryInstances.scala
+++ generic-scalacheck/shared/src/main/scala/com/peknight/generic/scalacheck/instances/ArbitraryInstances.scala
@@ -6,9 +6,16 @@ import org.scalacheck.{Gen, Arbitrary}
 
 trait ArbitraryInstances:
 
-  given productInstance[A](using generic: Generic.Product[Arbitrary, A]): Arbitrary[A] = generic.fromInstances
-  given sumInstance[A](using generic: Generic.Sum[Arbitrary, A]): Arbitrary[A] =
-    Arbitrary(Gen.choose(0, generic.size - 1).flatMap(i => generic.instance(i).arbitrary))
+  //given productInstance[A](using generic: => Generic.Product[Arbitrary, A]): Arbitrary[A] = generic.fromInstances
+
+  //given sumInstance[A](using generic: => Generic.Sum[Arbitrary, A]): Arbitrary[A] =
+  //  Arbitrary(Gen.choose(0, generic.size - 1).flatMap(i => generic.instance(i).arbitrary))
+
+  inline given derived[A](using generic: => Generic[Arbitrary, A]): Arbitrary[A] = generic.derive(
+    gen ?=> gen.fromInstances,
+    gen ?=> Arbitrary(Gen.choose(0, gen.size - 1).flatMap(i => gen.instance(i).arbitrary))
+  )
+
 end ArbitraryInstances
 object ArbitraryInstances extends ArbitraryInstances
 
