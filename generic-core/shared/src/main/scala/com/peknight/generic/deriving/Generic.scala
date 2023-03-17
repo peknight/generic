fd8e9abaf5caaaa0e560349be2b2f6895fc65951
peknight <JKpeknight@gmail.com>
2023-03-17 18:20:43 +0800

    update Generic

--- generic-core/shared/src/main/scala/com/peknight/generic/deriving/Generic.scala
+++ generic-core/shared/src/main/scala/com/peknight/generic/deriving/Generic.scala
@@ -10,9 +10,6 @@ import scala.compiletime.{constValue, constValueTuple, summonAll}
 sealed trait Generic[A]:
   type Labels <: Tuple
   type Repr <: Tuple
-  type MirrorType <: Mirror.Labelled[A, Labels, Repr]
-
-  def mirror: MirrorType
   def size: Int
   def labels: Labels
 end Generic
@@ -20,32 +17,24 @@ end Generic
 object Generic:
   def apply[A](using generic: Generic[A]): Generic[A] = generic
 
-  sealed abstract class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple, MirrorType0 <: Mirror.Labelled[A, Labels0, Repr0]](
-    val mirror: MirrorType0, val size: Int, val labels: Labels0
-  ) extends Generic[A]:
+  sealed abstract class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple](val size: Int, val labels: Labels0) extends Generic[A]:
     type Labels = Labels0
     type Repr = Repr0
-    type MirrorType = MirrorType0
   end Aux
 
   object Aux:
-    def apply[A, Labels <: Tuple, Repr <: Tuple, MirrorType <: Mirror.Labelled[A, Labels, Repr]](
-      using generic: Generic.Aux[A, Labels, Repr, MirrorType]
-    ): Aux[A, Labels, Repr, MirrorType] = generic
+    def apply[A, Labels <: Tuple, Repr <: Tuple](using generic: Generic.Aux[A, Labels, Repr]): Aux[A, Labels, Repr] =
+      generic
   end Aux
 
   sealed trait Instances[F[_], A]:
     type Labels <: Tuple
     type Repr <: Tuple
-    type MirrorType <: Mirror.Labelled[A, Labels, Repr]
-    type GenericType <: Generic.Aux[A, Labels, Repr, MirrorType]
+    type GenericType <: Generic.Aux[A, Labels, Repr]
     def generic: GenericType
     def instances: Map[Repr, F]
-
-    def mirror: MirrorType = generic.mirror
     def size: Int = generic.size
     def labels: Labels = generic.labels
-
     inline def derive(f: => Generic.Product.Instances[F, A] ?=> F[A], g: => Generic.Sum.Instances[F, A] ?=> F[A]): F[A] =
       inline this match
         case p: Generic.Product.Instances[F, A] => f(using p)
@@ -55,40 +44,40 @@ object Generic:
 
   object Instances:
     def apply[F[_], A](using inst: Instances[F, A]): Generic.Instances[F, A] = inst
-    sealed abstract class Aux[F[_], A, Labels0 <: Tuple, Repr0 <: Tuple,
-      MirrorType0 <: Mirror.Labelled[A, Labels0, Repr0], GenericType0 <: Generic.Aux[A, Labels0, Repr0, MirrorType0]](
+    sealed abstract class Aux[F[_], A, Labels0 <: Tuple, Repr0 <: Tuple, GenericType0 <: Generic.Aux[A, Labels0, Repr0]](
       val generic: GenericType0, instances0: () => Map[Repr0, F]
     ) extends Generic.Instances[F, A]:
       type Labels = Labels0
       type Repr = Repr0
-      type MirrorType = MirrorType0
       type GenericType = GenericType0
       lazy val instances: Map[Repr, F] = instances0()
     end Aux
     object Aux:
-      def apply[F[_], A, Labels <: Tuple, Repr <: Tuple, MirrorType <: Mirror.Labelled[A, Labels, Repr],
-        GenericType <: Generic.Aux[A, Labels, Repr, MirrorType]](
-        using inst: Generic.Instances.Aux[F, A, Labels, Repr, MirrorType, GenericType]
-      ): Aux[F, A, Labels, Repr, MirrorType, GenericType] = inst
+      def apply[F[_], A, Labels <: Tuple, Repr <: Tuple, GenericType <: Generic.Aux[A, Labels, Repr]](
+        using inst: Generic.Instances.Aux[F, A, Labels, Repr, GenericType]
+      ): Aux[F, A, Labels, Repr, GenericType] = inst
     end Aux
   end Instances
 
   sealed trait Product[A] extends Generic[A]:
-    type MirrorType = Mirror.Product.Labelled[A, Labels, Repr]
     def to(a: A): Repr
-    def from(repr: Repr): A = mirror.fromProduct(repr)
+    def from(repr: Repr): A
   end Product
 
   object Product:
     def apply[A](using generic: Generic.Product[A]): Generic.Product[A] = generic
 
-    abstract class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple](
-      override val mirror: Mirror.Product.Labelled[A, Labels0, Repr0],
+    sealed abstract class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple](override val size: Int, override val labels: Labels0)
+      extends Generic.Aux[A, Labels0, Repr0](size, labels) with Generic.Product[A]
+
+    final class MirrorAux[A <: scala.Product, Labels0 <: Tuple, Repr0 <: Tuple](
+      mirror: Mirror.Product.Labelled[A, Labels0, Repr0],
       override val size: Int,
       override val labels: Labels0
-    ) extends Generic.Aux[A, Labels0, Repr0, Mirror.Product.Labelled[A, Labels0, Repr0]](
-      mirror, size, labels
-    ) with Generic.Product[A]
+    ) extends Generic.Product.Aux[A, Labels0, Repr0](size, labels):
+      def to(a: A): Repr = Tuple.fromProduct(a).asInstanceOf[Repr]
+      def from(repr: Repr): A = mirror.fromProduct(repr)
+    end MirrorAux
 
     object Aux:
       def apply[A, Labels <: Tuple, Repr <: Tuple](using generic: Generic.Product.Aux[A, Labels, Repr])
@@ -96,7 +85,6 @@ object Generic:
     end Aux
 
     sealed trait Instances[F[_], A] extends Generic.Instances[F, A]:
-      type MirrorType = Mirror.Product.Labelled[A, Labels, Repr]
       type GenericType = Generic.Product.Aux[A, Labels, Repr]
       def fromInstances(using Applicative[F]): F[A] = instances.mapN(from)
       def map[G[_]](a: A)(f: [T] => (F[T], T) => G[T]): Map[Repr, G] =
@@ -135,8 +123,9 @@ object Generic:
       final class Aux[F[_], A, Labels <: Tuple, Repr <: Tuple](
         override val generic: Generic.Product.Aux[A, Labels, Repr],
         instances0: () => Map[Repr, F]
-      ) extends Generic.Instances.Aux[F, A, Labels, Repr, Mirror.Product.Labelled[A, Labels, Repr],
-        Generic.Product.Aux[A, Labels, Repr]](generic, instances0) with Generic.Product.Instances[F, A]
+      ) extends Generic.Instances.Aux[F, A, Labels, Repr, Generic.Product.Aux[A, Labels, Repr]](
+        generic, instances0
+      ) with Generic.Product.Instances[F, A]
       object Aux:
         def apply[F[_], A, Labels <: Tuple, Repr <: Tuple](
           using inst: Generic.Product.Instances.Aux[F, A, Labels, Repr]
@@ -146,8 +135,7 @@ object Generic:
   end Product
 
   sealed trait Sum[A] extends Generic[A]:
-    type MirrorType = Mirror.Sum.Labelled[A, Labels, Repr]
-    def ordinal(a: A): Int = mirror.ordinal(a)
+    def ordinal(a: A): Int
     def label(ord: Int): String = labels.productElement(ord).asInstanceOf[String]
     def label(a: A): String = label(ordinal(a))
   end Sum
@@ -155,13 +143,20 @@ object Generic:
   object Sum:
     def apply[A](using generic: Generic.Sum[A]): Generic.Sum[A] = generic
 
-    final class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple](
-      override val mirror: Mirror.Sum.Labelled[A, Labels0, Repr0],
+    sealed abstract class Aux[A, Labels0 <: Tuple, Repr0 <: Tuple](
       override val size: Int,
       override val labels: Labels0
-    ) extends Generic.Aux[A, Labels0, Repr0, Mirror.Sum.Labelled[A, Labels0, Repr0]](
-      mirror, size, labels
+    ) extends Generic.Aux[A, Labels0, Repr0](
+      size, labels
     ) with Generic.Sum[A]
+
+    final class MirrorAux[A, Labels0 <: Tuple, Repr <: Tuple](
+      mirror: Mirror.Sum.Labelled[A, Labels0, Repr],
+      override val size: Int,
+      override val labels: Labels0
+    ) extends Generic.Sum.Aux[A, Labels0, Repr](size, labels):
+      def ordinal(a: A): Int = mirror.ordinal(a)
+    end MirrorAux
 
     object Aux:
       def apply[A, Labels <: Tuple, Repr <: Tuple](using generic: Generic.Sum.Aux[A, Labels, Repr])
@@ -169,7 +164,6 @@ object Generic:
     end Aux
 
     sealed trait Instances[F[_], A] extends Generic.Instances[F, A]:
-      type MirrorType = Mirror.Sum.Labelled[A, Labels, Repr]
       type GenericType = Generic.Sum.Aux[A, Labels, Repr]
       def instance(ord: Int): F[A] = instances.productElement(ord).asInstanceOf[F[A]]
       def instance(a: A): F[A] = instance(ordinal(a))
@@ -183,7 +177,6 @@ object Generic:
       def ordinal(a: A): Int = generic.ordinal(a)
       def label(ord: Int): String = generic.label(ord)
       def label(a: A): String = generic.label(a)
-
     end Instances
 
     object Instances:
@@ -191,8 +184,9 @@ object Generic:
       final class Aux[F[_], A, Labels <: Tuple, Repr <: Tuple](
         override val generic: Generic.Sum.Aux[A, Labels, Repr],
         instances0: () => Map[Repr, F]
-      ) extends Generic.Instances.Aux[F, A, Labels, Repr, Mirror.Sum.Labelled[A, Labels, Repr],
-        Generic.Sum.Aux[A, Labels, Repr]](generic, instances0) with Generic.Sum.Instances[F, A]
+      ) extends Generic.Instances.Aux[F, A, Labels, Repr, Generic.Sum.Aux[A, Labels, Repr]](
+        generic, instances0
+      ) with Generic.Sum.Instances[F, A]
       object Aux:
         def apply[F[_], A, Labels <: Tuple, Repr <: Tuple](using inst: Generic.Sum.Instances.Aux[F, A, Labels, Repr])
         : Generic.Sum.Instances.Aux[F, A, Labels, Repr] = inst
@@ -202,14 +196,13 @@ object Generic:
 
   inline given [A <: scala.Product, Labels <: Tuple, Repr0 <: Tuple](
     using mirror: Mirror.Product.Labelled[A, Labels, Repr0]
-  ): Generic.Product.Aux[A, Labels, Repr0] =
-    new Generic.Product.Aux[A, Labels, Repr0](mirror, constValue[Size[Repr0]], constValueTuple[Labels]):
-      def to(a: A): Repr = Tuple.fromProduct(a).asInstanceOf[Repr]
+  ): Generic.Product.MirrorAux[A, Labels, Repr0] =
+    new Generic.Product.MirrorAux[A, Labels, Repr0](mirror, constValue[Size[Repr0]], constValueTuple[Labels])
 
   inline given [A, Labels <: Tuple, Repr <: Tuple](
     using mirror: Mirror.Sum.Labelled[A, Labels, Repr]
-  ): Generic.Sum.Aux[A, Labels, Repr] =
-    new Generic.Sum.Aux[A, Labels, Repr](mirror, constValue[Size[Repr]], constValueTuple[Labels])
+  ): Generic.Sum.MirrorAux[A, Labels, Repr] =
+    new Generic.Sum.MirrorAux[A, Labels, Repr](mirror, constValue[Size[Repr]], constValueTuple[Labels])
 
   inline given [F[_], A, Labels <: Tuple, Repr <: Tuple](using generic: Generic.Product.Aux[A, Labels, Repr])
   : Generic.Product.Instances.Aux[F, A, Labels, Repr] =
