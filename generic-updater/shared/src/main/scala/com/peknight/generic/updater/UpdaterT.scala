1586bf27dc52778c50f4000bad384f69633dd750
peknight <JKpeknight@gmail.com>
2023-03-21 23:05:41 +0800

    updater

--- generic-updater/shared/src/main/scala/com/peknight/generic/updater/UpdaterT.scala
+++ generic-updater/shared/src/main/scala/com/peknight/generic/updater/UpdaterT.scala
@@ -1,5 +1,8 @@
 package com.peknight.generic.updater
 
+import com.peknight.generic.updater.instances.IdInstances
+
 trait UpdaterT[F[_], A, B]:
   def update(a: A, b: B): F[A]
 end UpdaterT
+object UpdaterT extends IdInstances
