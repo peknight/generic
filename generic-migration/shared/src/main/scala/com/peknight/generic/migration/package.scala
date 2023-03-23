07dfec83884ff0c8ea402389e8fe8da85a106452
peknight <JKpeknight@gmail.com>
2023-03-23 11:24:34 +0800

    update migration

--- generic-migration/shared/src/main/scala/com/peknight/generic/migration/package.scala
+++ generic-migration/shared/src/main/scala/com/peknight/generic/migration/package.scala
@@ -1,11 +1,7 @@
 package com.peknight.generic
 
-import cats.data.Kleisli
 import cats.Id
 
 package object migration:
-
-  // type MigrationT[F[_], -A, B] = Kleisli[F, A, B]
   type Migration[-A, B] = MigrationT[Id, A, B]
-
 end migration
