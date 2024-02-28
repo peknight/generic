15a41bd77bf51d8da0c39bf2b4847948db82d9ca
peknight <JKpeknight@gmail.com>
2024-02-28 16:19:45 +0800

    update applicative instances

--- generic-instances/squants/shared/src/main/scala/com/peknight/generic/instances/squants/market/MoneyInstances.scala
+++ generic-instances/squants/shared/src/main/scala/com/peknight/generic/instances/squants/market/MoneyInstances.scala
@@ -1,13 +1,14 @@
 package com.peknight.generic.instances.squants.market
 
-import cats.Id
-import com.peknight.generic.migration.id.Isomorphism
+import cats.Applicative
+import cats.syntax.applicative.*
+import com.peknight.generic.migration.Isomorphism
 import squants.market.{Money, MoneyContext}
 
 trait MoneyInstances:
-  given (using context: MoneyContext): Isomorphism[Money, BigDecimal] with
-    def to(a: Money): Id[BigDecimal] = a.to(context.defaultCurrency)
-    def from(b: BigDecimal): Id[Money] = context.defaultCurrency(b)
+  given [F[_]](using applicative: Applicative[F], context: MoneyContext): Isomorphism[F, Money, BigDecimal] with
+    def to(a: Money): F[BigDecimal] = a.to(context.defaultCurrency).pure[F]
+    def from(b: BigDecimal): F[Money] = context.defaultCurrency(b).pure[F]
   end given
 end MoneyInstances
 object MoneyInstances extends MoneyInstances
--- generic-instances/time/shared/src/main/scala/com/peknight/generic/instances/time/YearInstances.scala
+++ generic-instances/time/shared/src/main/scala/com/peknight/generic/instances/time/YearInstances.scala
@@ -1,14 +1,15 @@
 package com.peknight.generic.instances.time
 
-import cats.Id
-import com.peknight.generic.migration.id.Isomorphism
+import cats.Applicative
+import cats.syntax.applicative.*
+import com.peknight.generic.migration.Isomorphism
 
 import java.time.Year
 
 trait YearInstances:
-  given Isomorphism[Year, Int] with
-    def to(a: Year): Id[Int] = a.getValue
-    def from(b: Int): Id[Year] = Year.of(b)
+  given [F[_]: Applicative]: Isomorphism[F, Year, Int] with
+    def to(a: Year): F[Int] = a.getValue.pure[F]
+    def from(b: Int): F[Year] = Year.of(b).pure[F]
   end given
 end YearInstances
 object YearInstances extends YearInstances
